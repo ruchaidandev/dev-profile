f49301061dc78bfa2e4b340e88cf464ef7a842e6
Aidan Perera <ruchaidan@gmail.com>
2022-01-23 12:28:52 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? "/" : "",
   images: {
     loader: 'akamai',
     path: '', 
