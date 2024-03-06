41f4586adbcb826b13c633947050493a2303174d
Aidan Perera <ruchaidan@gmail.com>
2024-03-06 10:34:55 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
    output: 'export',
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     formats: ["image/webp"],
   },
